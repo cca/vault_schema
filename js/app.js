a9a9cfde4930bc7f650fec7e2a12f988c7913921
phette23 <phette23@gmail.com>
2015-06-10 09:53:30 -0700

    unnecessary console.log calls

--- js/app.js
+++ js/app.js
@@ -2,15 +2,13 @@ $('.js-expand-all').click(expandAll)
 $('.js-collapse-all').click(collapseAll)
 
 function expandAll (ev) {
-    console.log('expanded')
-    $('.parent_li').each(function (parent) {
+    $('.parent_li').each(function() {
         $(this).find('> ul > li').show('fast')
     })
 }
 
 function collapseAll (ev) {
-    console.log('collapsed')
-    $('.parent_li').each(function (parent) {
+    $('.parent_li').each(function() {
         $(this).find('> ul > li').hide('fast')
     })
 }
